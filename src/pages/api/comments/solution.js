4a45f914710f32989606b97e3ed177660541fa57
“Apspelet33” <williamstarwars1@hotmail.com>
2022-09-23 11:36:28 +0200

    solution

---
+++ src/pages/api/comments/solution.js
@@ -0,0 +1,24 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Comment from "models/comment";
+import connectDB from "utils/connectDB";
+import { unstable_getServerSession } from "next-auth/next";
+
+export default async function handler(req, res) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
+
+  if (req.method === "POST") {
+    await connectDB();
+
+    Comment.findOneAndModify(
+      { _id: req.body.cid },
+      { solution: req.body.solution }
+    );
+    await res.revalidate(`/questions/${req.body.qid}`);
+    return res.status(200).send({ message: "1 doc modified" });
+  } else return res.status(418).send({ message: "invalid method" });
+}
