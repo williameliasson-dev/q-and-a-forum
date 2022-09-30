b3f5c5319831e998c6a90ed2077fa666b1ee4086
“Apspelet33” <williamstarwars1@hotmail.com>
2022-09-30 14:33:46 +0200

    search works

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,36 +1,48 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import styles from "./SearchBar.module.scss";
 import useSWR from "swr";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
-  const [shouldFetch, setShouldFetch] = useState("");
-
+  const [shouldFetch, setShouldFetch] = useState(true);
+  const [results, setResults] = useState([]);
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  let { data: comments, error } = useSWR(
-    () => (shouldFetch ? "/api/data" : null),
-    fetcher
+  let { data: fetchedResults, error } = useSWR(
+    () => (shouldFetch ? `/api/search/get?title=${search}` : null),
+    fetcher,
+    { refreshInterval: 100 }
   );
 
   useEffect(() => {
-    setShouldFetch(true);
-    if (shouldFetch === true) {
-      setShouldFetch(!shouldFetch);
+    if (fetchedResults && shouldFetch) {
+      setResults(fetchedResults);
     }
-  }, [search]);
+  }, [search, fetchedResults]);
 
   return (
     <div>
       <form>
         <div className={styles.searchbar}>
-          <img src="/search.svg" className="h-6"></img>
-          <input
-            onChange={(e) => setSearch(e.target.value)}
-            value={search}
-            placeholder="Search..."
-            type="text"
-          />
+          <div className={styles.search}>
+            <img src="/search.svg" className="h-6"></img>
+            <input
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              placeholder="Search..."
+              type="text"
+            />
+          </div>
+          <div className={styles.results}>
+            {results?.map((result, i) => {
+              return (
+                <div key={i}>
+                  <Link href={`/questions/${result._id}`}>{result.title}</Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </form>
     </div>
