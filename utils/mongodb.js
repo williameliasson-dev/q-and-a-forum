9c9d994109072af31ff638938417365b463e9956
“Apspelet33” <williamstarwars1@hotmail.com>
2022-10-03 11:02:35 +0200

    mongodb

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -16,11 +16,11 @@ if (!process.env.MONGODB_URI) {
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
